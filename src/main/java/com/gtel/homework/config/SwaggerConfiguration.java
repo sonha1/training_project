12bb4ea93adcbea3f7095bdaf7768e620bc59bfb
sontk <sontk@sphinxjsc.com>
2025-04-14 17:32:41 +0700

    update swagger config

--- src/main/java/com/gtel/homework/config/SwaggerConfiguration.java
+++ src/main/java/com/gtel/homework/config/SwaggerConfiguration.java
@@ -1,7 +1,12 @@
 package com.gtel.homework.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.*;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +17,38 @@ public class SwaggerConfiguration {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("My Awesome API")
-                        .version("v1.0")
-                        .description("Tài liệu API cho ứng dụng Spring Boot 3"));
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI().components(new Components()
+                .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                )
+                .addSchemas("ApiErrorResponse", new ObjectSchema()
+                        .addProperty("status", new IntegerSchema())
+                        .addProperty("code", new IntegerSchema())
+                        .addProperty("message", new StringSchema())
+                        .addProperty("fieldErrors", new ArraySchema()
+                                .items(new Schema<ArraySchema>()
+                                        .$ref("ApiErrorResponse"))))
+                .addSchemas("ApiFieldError", new ObjectSchema()
+                        .addProperty("code", new StringSchema())
+                        .addProperty("message", new StringSchema())
+                        .addProperty("property", new StringSchema())
+                        .addProperty("path", new StringSchema())
+                        .addProperty("rejectedValue", new ObjectSchema())
+                ));
+    }
+
+    @Bean
+    public OperationCustomizer operationCustomizer() {
+        return (operation, handlerMethod) -> {
+            operation.getResponses().addApiResponse("4xx/5xx", new ApiResponse()
+                    .description("Error")
+                    .content(new Content().addMediaType("*/*", new MediaType().schema(new Schema<MediaType>().$ref("ApiErrorResponse")))));
+            return operation;
+        };
     }
 
 }
